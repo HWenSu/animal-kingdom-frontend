3c9befbcf7f50048c0fd7d2e16752f0ea7fb1c1c
Elva Su <112391401+HWenSu@users.noreply.github.com>
2025-08-17 12:39:13 +0800

    Feat - 設置送養頁面樣式

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,12 +1,11 @@
-import AdoptionForm from "./AdoptionForm"
-
+import AdoptionForm from "./AdoptionForm";
 
 const PostPage = () => {
   return (
-    <div>
-      <AdoptionForm isPost={true}/>
+    <div className="post-page">
+      <AdoptionForm isPost={true} />
     </div>
   );
-}
+};
 
-export default PostPage
+export default PostPage;
